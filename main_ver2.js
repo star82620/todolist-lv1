18b55fcb1064c0c67317bd3f054336cadf5d9ad9
AYU <star82620@gmail.com>
2023-03-30 16:47:47 +0800

    update/ fixed action button function

--- main_ver2.js
+++ main_ver2.js
@@ -1,20 +1,23 @@
 window.addEventListener("load", () => {
-    const form = document.querySelector("#new_task_form");
-    const input = document.querySelector("#new_task_input");
+    const newTaskForm = document.querySelector("#new_task_form");
+    const newTaskInput = document.querySelector("#new_task_input");
     const list_el = document.querySelector("#tasks");
     
+    const LOCAL_STORAGE_TASK_KEY = "ayuTask";
     //task 內容陣列 => 之後的 local storage
-    let tasksData = [
-        { value: "edit to-do list" }, 
-        { value: "heheheh" },
-        { value: "HBD" }
-    ];
-    
-    // 新增一組 task 的結構 
-    const buildTaskList = (data) => {
-        let tasksList = ""; //全部 task 的 HTML
-        data.forEach(function(item, index) {      
-            const taskValue = `
+    // const tasksData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || [];
+    const tasksData =[
+        {id:1, value: "todolist"},
+        {id:2, value: "bibibib"},
+        {id:3, value: "HEHEHE"}];
+
+    console.log(tasksData);
+    // 讀取生成所有 task 結構 
+    const render = () => {
+        let taskHTML = ""; //全部 task 的 HTML
+
+        tasksData.forEach(function(item, index) {      
+            const taskElement = `
                 <div class="task">
                     <div class="check">
                         <input type="checkbox" class="do">
@@ -27,27 +30,40 @@ window.addEventListener("load", () => {
                         <button class="delete" data-id="${index}">Delete</button>
                     </div>
                 </div>`;
-            tasksList+=taskValue;
-            // tasksList = tasksList + taskComp;
+            taskHTML+=taskElement;
+            // taskHTML = taskHTML + taskComp;
         });
-        list_el.innerHTML =  tasksList; //全部再一起插入
+        list_el.innerHTML =  taskHTML; //全部再一起插入
     };
+
+    // 將資料轉變成 JSON 格式並傳給 localStorage
+    const saveData = () => {
+        // localStorage.setItem(LOCAL_STORAGE_TASK_KEY, JSON.stringify(tasksData));
+    };
+
+
     
-    buildTaskList(tasksData);
+    render();
     
     //----新增 task
-    form.addEventListener("submit", function(e) {
+    newTaskForm.addEventListener("submit", function(e) {
         e.preventDefault();
         
-        if (input.value === "") {
+        if (newTaskInput.value === "") {
             alert("請輸入文字");
             return;
         };
 
-        tasksData.push({value: `${input.value}`});
-        buildTaskList(tasksData);
+        tasksData.push({
+            id: Date.now().toString(), 
+            value: `${newTaskInput.value}`,
+            done: ""
+            });
 
-        input.value = "";
+        saveData();
+        render();
+
+        newTaskInput.value = "";
     });
 
 
@@ -57,20 +73,18 @@ window.addEventListener("load", () => {
         const taskDo = taskBox.querySelector(".do");
         
         if(e.target.classList.contains("do")) {
-            console.log(e.target.checked);
+            
+
             if(e.target.checked) {
                 taskContent.classList.add("done");
-                console.log("hit checked");
             } else {
                 taskContent.classList.remove("done");
             }
         };
 
         if(e.target.classList.contains("edit")){
-            console.log("id: "+e.target.dataset.id);
 
             if(e.target.innerText.toLowerCase() === "edit"){
-                console.log("is edit"+"id: "+e.target.dataset.id);
                 e.target.innerText = "save";
                 taskContent.removeAttribute("readonly");
                 taskContent.focus();
@@ -78,24 +92,23 @@ window.addEventListener("load", () => {
                 taskBox.style.backgroundColor = "var(--darker)";
                 taskDo.style.visibility = "hidden";
             } else if (e.target.innerText.toLowerCase() === "save") {
-                console.log("is save"+"id: "+e.target.dataset.id);
                 e.target.innerText = "edit";
                 taskContent.setAttribute("readonly","readonly");
                 taskBox.style.backgroundColor = "var(--darkest)";
                 taskDo.style.visibility = "visible";
+
                 tasksData[e.target.dataset.id].value = taskContent.value;
-                // buildTaskList(tasksData); //是這傢伙有問題！
+                saveData();
+                render();
             };
 
-            return;
         };
 
         if(e.target.innerText.toLowerCase() === "delete"){
             tasksData.splice(e.target.dataset.id, 1);
-            buildTaskList(tasksData);
-            console.log(tasksData);
-            console.log("delete");
-            
+
+            saveData();
+            render();            
         };
 
 
