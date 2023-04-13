8a44c296b526f69b5874f672f9df2fda067218b3
AYU <star82620@gmail.com>
2023-04-13 11:03:11 +0800

    kata/ 20230413 1040-1057(17)

--- main_kata0413.js
+++ main_kata0413.js
@@ -1,56 +1,57 @@
-//2023/04/12 1505-1522
+//20230413 10:40-10:57 (17)
 
 // const tasksData = [
-//     {id:1, value:"A", isDone:false},
-//     {id:2, value:"A", isDone:false},
-// ];
+//     {id:1, value:"ddd", isDone:false},
+//     {id:2, value:"adawdaddd", isDone:true}
 
-const LOCAL_STORAGE_KEY = "ayuTaskKata0412";
-const tasksData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+// ]
 
 const tasksList = document.querySelector("#tasks");
+const newTaskForm = document.querySelector("#new_task_form");
+const newTaskInput = document.querySelector("#new_task_input");
 
-//13-14
+//47-49
+const LOCAL_STORAGE_TASK_KEY = "ayuTaskKata0413";
+const tasksData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || [];
 const saveData = () => {
-    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(tasksData));
+    localStorage.setItem(LOCAL_STORAGE_TASK_KEY, JSON.stringify(tasksData));
 };
 
-//C"R"UD 1505-09
+
+//C"R"UD 40-44
 const render = () => {
     let tasksHTML = "";
     tasksData.forEach((item) => {
         const isDone = item.isDone ? "done" : "";
         const isChecked = item.isDone ? "checked" : "";
+
         const taskElement = 
-        `<div class="task ${isDone}">
-            <div class="check">
-                <input type="checkbox" class="do" data-id="${item.id}" ${isChecked}>
-            </div>
-            <div class="content">
-                <input type="text" class="text" value="${item.value}" data-id="${item.id}" readonly>
-            </div>
-            <div class="actions">
-                <button class="edit" data-id="${item.id}">Edit</button>
-                <button class="delete" data-id="${item.id}">Delete</button>
-            </div>
-        </div>`;
+            `<div class="task ${isDone}">
+                <div class="check">
+                    <input type="checkbox" class="do" data-id="${item.id}" ${isChecked}>
+                </div>
+                <div class="content">
+                    <input type="text" class="text" value="${item.value}" data-id="${item.id}" readonly>
+                </div>
+                <div class="actions">
+                    <button class="edit" data-id="${item.id}">Edit</button>
+                    <button class="delete" data-id="${item.id}">Delete</button>
+                </div>
+            </div>`;
         tasksHTML += taskElement;
     });
+
     tasksList.innerHTML = tasksHTML;
 };
 
 render();
 
 
-//"C"RUD 09-11
-const newTaskForm = document.querySelector("#new_task_form");
-const newTaskInput = document.querySelector("#new_task_input");
-
+//"C"RUD 44-47
 newTaskForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if(newTaskInput.value === "") return alert("Please type something");
-
     const taskValue = {
         id: Date.now().toString(),
         value: newTaskInput.value,
@@ -63,28 +64,30 @@ newTaskForm.addEventListener("submit", (e) => {
     render();
 
     newTaskInput.value = "";
+
 });
 
+//CR"UD" 49-57
 
-//CR"UD" 14-22
 tasksList.addEventListener("click", (e) => {
     const taskWarp = e.target.parentNode.parentNode;
     const taskText = taskWarp.querySelector(".text");
     const taskId = e.target.dataset.id;
-    const taskIndex = tasksData.findIndex((item)=>item.id === taskId);
+    const taskIndex = tasksData.findIndex((item) => item.id === taskId);
 
-    if(e.target.classList.contains("do")){ //20-22
+    if(e.target.classList.contains("do")){ //55-57
         if(e.target.checked) {
             tasksData[taskIndex].isDone = true;
             taskWarp.classList.add("done");
+            
         } else {
             tasksData[taskIndex].isDone = false;
             taskWarp.classList.remove("done");
-        }
+        };
         saveData();
         render();
-    } else if(e.target.classList.contains("edit")){ //15-20
-        if(e.target.innerText.toLowerCase() ==="edit") {
+    } else if (e.target.classList.contains("edit")) { //50-55
+        if(e.target.innerText.toLowerCase() === "edit") {
             e.target.innerText = "save";
             taskText.removeAttribute("readonly");
             taskText.focus();
@@ -93,11 +96,11 @@ tasksList.addEventListener("click", (e) => {
             e.target.innerText = "edit";
             taskText.setAttribute("readonly", "readonly");
             tasksData[taskIndex].value = taskText.value;
-
             saveData();
             render();
-        }
-    } else if(e.target.classList.contains("delete")){ //20-20
+        };
+        
+    } else if (e.target.classList.contains("delete")) { //55-55
         tasksData.splice(taskIndex, 1);
         saveData();
         render();
