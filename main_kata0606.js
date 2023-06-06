f5b5c08104b9ab6a993780e8c2b9317e69e4bee9
Ayu Huang <star82620@gmail.com>
2023-06-06 10:03:07 +0800

    kata/ 20230606 0928-1001(33)

--- main_kata0606.js
+++ main_kata0606.js
@@ -1,33 +1,33 @@
-//2023/05/30 0935 - 0955 (20)
+//20230606 0928-1001
 
-//C"R"UD 0935-39
-const tasksList = document.querySelector("#tasks");
-
-const LOCAL_STORAGE_KEY = "ayu_kata0530";
+//C"R"UD 0928-0932
+const LOCAL_STORAGE_KEY = "ayu_kata0606";
 const tasksData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
 const saveData = () => {
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasksData));
 };
+const tasksList = document.querySelector("#tasks");
+
 const render = () => {
-  let tasksHTML = "";
+  let taskHTML = "";
   tasksData.forEach((item) => {
     const isDone = item.done ? "done" : "";
     const isChecked = item.done ? "checked" : "";
     const taskElement = `<div class="task ${isDone}">
-        <div class="check">
-          <input type="checkbox" class="do" data-id="${item.id}" ${isChecked}>
-        </div>
-        <div class="content">
-          <input type="text" class="text" value="${item.value}" data-id="${item.id}" readonly>
-        </div>
-        <div class="actions">
-          <button class="edit" data-id="${item.id}">Edit</button>
-          <button class="delete" data-id="${item.id}">Delete</button>
-        </div>
-      </div>`;
-    tasksHTML += taskElement;
+          <div class="check">
+            <input type="checkbox" class="do" data-id="${item.id}" ${isChecked}>
+          </div>
+          <div class="content">
+            <input type="text" class="text" value="${item.value}" data-id="${item.id}" readonly>
+          </div>
+          <div class="actions">
+            <button class="edit" data-id="${item.id}">Edit</button>
+            <button class="delete" data-id="${item.id}">Delete</button>
+          </div>
+        </div>`;
+    taskHTML += taskElement;
   });
-  tasksList.innerHTML = tasksHTML;
+  tasksList.innerHTML = taskHTML;
 };
 
 const saveAndRender = () => {
@@ -37,7 +37,8 @@ const saveAndRender = () => {
 
 saveAndRender();
 
-//"C"RUD 0939-42
+//"C"RUD
+
 const newTaskForm = document.querySelector("#new_task_form");
 const newTaskInput = document.querySelector("#new_task_input");
 
@@ -50,13 +51,18 @@ newTaskForm.addEventListener("submit", (e) => {
     value: newTaskInput.value,
     done: false,
   };
-  newTaskInput.value = "";
 
   tasksData.push(taskValue);
   saveAndRender();
+
+  newTaskInput.value = "";
 });
 
-//CR"UD" 0944-0955
+//CR"UD" ????
+
+// const
+console.log("FFF");
+
 tasksList.addEventListener("click", (e) => {
   const btnClass = e.target.classList;
   const taskWarp = e.target.parentNode.parentNode;
@@ -65,9 +71,6 @@ tasksList.addEventListener("click", (e) => {
   const taskIndex = tasksData.findIndex((item) => {
     return item.id === taskId;
   });
-
-  console.log(taskId);
-  console.log(taskIndex);
 
   if (btnClass.contains("do")) {
     const isBtnChecked = e.target.checked;
@@ -91,36 +94,5 @@ tasksList.addEventListener("click", (e) => {
   if (btnClass.contains("delete")) {
     tasksData.splice(taskIndex, 1);
     saveAndRender();
-  }
-});
-
-//CR"UD" 0940-
-
-tasksList.addEventListener("click", (e) => {
-  const btnClass = e.target.classList;
-  const taskWarp = e.target.parentNode.parentNode;
-  const taskText = taskWarp.querySelector(".text");
-  const taskId = e.target.dataset.id;
-  const taskIndex = tasksData.findIndex((item) => {
-    return (item.id = taskId);
-  });
-  console.log("FE");
-
-  if (btnClass.contains("do")) {
-  }
-  if (btnClass.contains("edit")) {
-    if (e.target.innerText.toLowerCase() === "edit") {
-      e.target.innerText = "save";
-      taskText.removeAttribute("readonly");
-      taskText.focus();
-      taskText.setSelectionRange(999, 999);
-    } else {
-      e.target.innerText = "edit";
-      taskText.setAttribute("readonly", "readonly");
-      tasksData[taskIndex].value = taskText.value;
-      saveAndRender();
-    }
-  }
-  if (btnClass.contains("delete")) {
   }
 });
